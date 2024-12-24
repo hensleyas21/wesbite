74d60317ab661401879cc48d5bff0b4a3450c1bd
Liam Grossman <liam.j.grossman@gmail.com>
2024-12-24 13:44:35 -0500

    Update icons

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -10,7 +10,7 @@ interface IExperienceProps {
 export default function Experience(props: IExperienceProps) {
   const { company, position, description, linkedin } = props;
   return (
-    <div className="flex flex-row justify-between items-center group space-x-3">
+    <div className="flex flex-row justify-between items-center space-x-5">
       <div className="space-y-2">
         <h2 className="font-medium">
           {position}, {company}
@@ -19,7 +19,7 @@ export default function Experience(props: IExperienceProps) {
       </div>
       {linkedin && (
         <a href={linkedin} target="_blank" rel="noopener noreferrer">
-          <Linkedin className="group-hover:opacity-100 opacity-0 w-5 h-5 transition-opacity" />
+          <Linkedin className="w-5 h-5 transition-opacity" />
         </a>
       )}
     </div>
--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -9,14 +9,14 @@ interface IProjectProps {
 export default function Project(props: IProjectProps) {
   const { name, description, github } = props;
   return (
-    <div className="flex flex-row justify-between items-center group space-x-3">
+    <div className="flex flex-row justify-between items-center space-x-5">
       <div className="space-y-2">
         <h2 className="font-medium">{name}</h2>
         <p>{description}</p>
       </div>
       {github && (
         <a href={github} target="_blank" rel="noopener noreferrer">
-          <GitHub className="group-hover:opacity-100 opacity-0 w-5 h-5 transition-opacity" />
+          <GitHub className="w-5 h-5 transition-opacity" />
         </a>
       )}
     </div>
